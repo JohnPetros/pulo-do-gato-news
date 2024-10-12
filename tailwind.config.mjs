f6fe68e52c12c471cb76d15ab568244d449f546c
JohnPetros <joaopedro.nc@outlook.com>
2024-10-12 13:31:59 -0300

    ⚙️ config: set theme colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,23 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#CA474D',
+				gray: {
+					900: '#0D0D0D',
+					800: '#262626',
+					700: '#404040',
+					600: '#595959',
+					500: '#737373',
+					400: '#8C8C8C',
+					300: '#A6A6A6',
+					200: '#BFBFBF',
+					100: '#D9D9D9',
+					50: '#F2F2F2',
+				}
+			},
+		},
 	},
 	plugins: [],
 }
