831d85eb78b98ca30ca7706c58f593a4ec162b86
JohnPetros <joaopedro.nc@outlook.com>
2024-10-13 09:36:42 -0300

    🧱 components: add post page

--- src/cms/sanity/constants/categories.ts
+++ src/cms/sanity/constants/categories.ts
@@ -1,8 +1,8 @@
 export const CATEGORIES = [
-  { title: 'tecnologia', value: 'tecnology' },
-  { title: 'política', value: 'politics' },
-  { title: 'educação', value: 'education' },
-  { title: 'mundo', value: 'world' },
-  { title: 'economia', value: 'economy' },
-  { title: 'cultura', value: 'culture' },
+  { title: 'tecnologia', value: 'tecnologia' },
+  { title: 'política', value: 'política' },
+  { title: 'educação', value: 'educação' },
+  { title: 'mundo', value: 'mundo' },
+  { title: 'economia', value: 'economia' },
+  { title: 'cultura', value: 'cultura' },
 ]
