cfcc83264bea0059a432b2ec11f9b52e3dc259ef
JohnPetros <joaopedro.nc@outlook.com>
2024-10-13 09:35:39 -0300

    📡 services: get post by slug

--- src/@core/interfaces/posts-service.ts
+++ src/@core/interfaces/posts-service.ts
@@ -2,4 +2,6 @@ import type { Post } from '../types'
 
 export interface PostsService {
   getLastPosts(): Promise<Post[]>
+  getPostSlugs(): Promise<string[]>
+  getPostBySlug(slug: string): Promise<Post>
 }
--- src/cms/sanity/services/sanity-posts-service.ts
+++ src/cms/sanity/services/sanity-posts-service.ts
@@ -1,14 +1,53 @@
 import { sanityClient } from 'sanity:client'
-import type { PostsService } from '../../../@core/interfaces'
-import type { Post } from '../../../@core/types'
+
+import type { PostsService } from '@/core/interfaces'
+import type { Post } from '@/core/types'
 
 export const SanityPostsService = (): PostsService => {
   return {
     async getLastPosts() {
-      const posts = await sanityClient.fetch(
-        '*[_type == "post"] | order(_createdAt desc){"slug": slug.current, name, author, date, category, "image": image.asset->url, content}',
+      const sanityPosts = await sanityClient.fetch(`
+        *[_type == "post"] | order(_createdAt desc)
+        {
+          name,
+          "slug": slug.current,
+          date,
+          category,
+          tags,
+          author,
+          content,
+          "image": image.asset->url
+        }
+        [0..3]
+        `)
+      return sanityPosts as Post[]
+    },
+
+    async getPostSlugs() {
+      const sanitySlugs = await sanityClient.fetch(
+        '*[_type == "post"]{"slug": slug.current}',
       )
-      return posts as Post[]
+      return (sanitySlugs as { slug: string }[]).map(({ slug }) => slug)
+    },
+
+    async getPostBySlug(slug: string) {
+      const sanityPost = await sanityClient.fetch(
+        `*[_type == "post" && slug.current == $slug][0]
+        {
+          name,
+          "slug": slug.current,
+          date,
+          category,
+          tags,
+          author,
+          content,
+          "image": image.asset->url
+        }
+        `,
+        { slug },
+      )
+
+      return sanityPost as Post
     },
   }
 }
