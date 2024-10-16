331be23771ba816149c34917bbfcd558595a12f4
JohnPetros <joaopedro.nc@outlook.com>
2024-10-15 21:36:22 -0300

    🪨 constants: add email regex

---
+++ src/constants/env.ts
@@ -0,0 +1,5 @@
+export const ENV = {
+  sanityToken: import.meta.env.SANITY_TOKEN,
+  sanityDataset: import.meta.env.SANITY_DATASET,
+  sanityProjectId: import.meta.env.SANITY_PROJECT_ID,
+}
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,4 @@
 export { NAV_LINKS } from './nav-links'
 export { PAGINATION } from './pagination'
+export { EMAIL_REGEX } from './email-regex'
+export { ENV } from './env'
