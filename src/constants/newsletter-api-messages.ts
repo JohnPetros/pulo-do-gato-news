62847707aef651076fb1b85c59b7d2e74580b0c3
JohnPetros <joaopedro.nc@outlook.com>
2024-10-16 22:04:46 -0300

    🪨 constants: add NEWSLETTER_API_MESSAGES

---
+++ src/constants/newsletter-api-messages.ts
@@ -0,0 +1,8 @@
+export const NEWSLETTER_API_MESSAGES = {
+  error: {
+    invalidEmail: 'E-mail inválido',
+    emailAlreasyExists: 'E-mail já registrado',
+    server: 'Erro inesperado ao registrar e-mail',
+  },
+  success: 'Sua inscrição foi registrada',
+}
