5a76b36b9d8e0e798204aa2f3dac73ac7ddbfd33
JohnPetros <joaopedro.nc@outlook.com>
2024-10-17 22:31:27 -0300

    🏷️ types: add comment

---
+++ src/@core/types/comment.ts
@@ -0,0 +1,6 @@
+export type Comment = {
+  name: string
+  email: string
+  content: string
+  postId: string
+}
--- src/@core/types/index.ts
+++ src/@core/types/index.ts
@@ -1,2 +1,3 @@
 export type { Category } from './category'
+export type { Comment } from './comment'
 export type { Post } from './post'
