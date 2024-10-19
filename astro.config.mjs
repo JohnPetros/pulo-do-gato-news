18de20072c8a57cb0dc45c231a55036afc17ca19
JohnPetros <joaopedro.nc@outlook.com>
2024-10-19 14:13:58 -0300

    ⚙️ config: remove experimental features from astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,8 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sanity from '@sanity/astro';
 
-
-
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-
-  },
   integrations: [tailwind(), react(), sanity(
     {
       projectId: 'jlfn5obv',
