3ee964c05aade24423211a5015a46ed7c40110f7
JohnPetros <joaopedro.nc@outlook.com>
2024-12-08 14:30:30 -0300

    🧱 templates: make button component a link when necessary

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -4,9 +4,25 @@ import { twMerge } from 'tailwind-merge'
 type Props = {
   children: ReactNode
   className?: string
+  href?: string
+  isLink?: boolean
 } & ComponentProps<'button'>
 
-export const Button = ({ children, className, ...buttonProps }: Props) => {
+export const Button = ({ children, isLink, className, href, ...buttonProps }: Props) => {
+  if (isLink) {
+    return (
+      <a
+        href={href}
+        className={twMerge(
+          'flex items-center justify-center gap-2 rounded-md px-3 py-2 bg-primary text-white  focus:ring-4 focus:ring-red-300 focus:outline-none hover:bg-red-800',
+          className,
+        )}
+      >
+        {children}
+      </a>
+    )
+  }
+
   return (
     <button
       {...buttonProps}
