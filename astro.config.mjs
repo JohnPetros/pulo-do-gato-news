78f49ded8839e1115c6398a8c822e6d70884414f
JohnPetros <joaopedro.nc@outlook.com>
2024-12-29 10:39:49 -0300

    📦 deps: install partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import sanity from '@sanity/astro';
-
 import netlify from '@astrojs/netlify';
+import partytown from '@astrojs/partytown';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sanity(
+  integrations: [tailwind(), react(), partytown(), sanity(
     {
       projectId: 'jlfn5obv',
       dataset: 'production',
