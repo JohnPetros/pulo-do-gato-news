95b34e8d048665435ffb1a16d42b741099bdb1d7
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-08-03 20:21:56 -0300

    🪨 constants: add HTTP_STATUS_CODE

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -2,4 +2,5 @@ export const ENV = {
   sanityToken: import.meta.env.SANITY_TOKEN,
   sanityDataset: import.meta.env.SANITY_DATASET,
   sanityProjectId: import.meta.env.SANITY_PROJECT_ID,
+  newsAiApiUrl: import.meta.env.NEWS_AI_API_URL,
 } as const
