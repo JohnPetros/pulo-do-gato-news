553dd133d119ca689cad0417740c57d4ec17fa66
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-01-04 13:01:22 -0300

    🪨 constants: add HTTP_HEADERS constant for standardized HTTP header values

---
+++ src/constants/http-headers.ts
@@ -0,0 +1,7 @@
+export const HTTP_HEADERS = {
+  StrictTransportSecurity: 'Strict-Transport-Security',
+  XFrameOptions: 'X-Frame-Options',
+  XContentTypeOptions: 'X-Content-Type-Options',
+  ReferrerPolicy: 'Referrer-Policy',
+  PermissionsPolicy: 'Permissions-Policy',
+} as const
